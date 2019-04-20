a02c3b78e99dbe899d8552be84a0a0ab162396b7
Robson Brito <robson.rmb@gmail.com>
2019-04-20 00:50:20 -0300

    Projeto criado

---
+++ src/main/java/br/com/ts/TopspinApplication.java
@@ -0,0 +1,13 @@
+package br.com.ts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopspinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopspinApplication.class, args);
+	}
+
+}
