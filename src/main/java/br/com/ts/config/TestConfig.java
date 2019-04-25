082272bd793d4b0dd475e9f6ea6ed5e37a29a94f
Robson Brito <robson.rmb@gmail.com>
2019-04-24 22:43:51 -0300

    Enviando email usando o servidor do google.

--- src/main/java/br/com/ts/config/TestConfig.java
+++ src/main/java/br/com/ts/config/TestConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import br.com.ts.service.mail.EmailService;
+import br.com.ts.service.mail.SmtpEmailService;
+
 @Configuration
 @Profile("test")
 public class TestConfig {
@@ -13,6 +16,11 @@ public class TestConfig {
 		
 		System.out.println("Inicializando profile: test!!!");
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 	
 }
